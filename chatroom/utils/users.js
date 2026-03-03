5e531a6d5e3ebd8997a8a2b2a2f4cbb206a757fb
Sachinpathak098 <124699279+Sachinpathak098@users.noreply.github.com>
2026-03-03 11:08:35 +0530

    users.js

--- chatroom/utils/users.js
+++ chatroom/utils/users.js
@@ -14,7 +14,7 @@ function getCurrentUser(id){
     return users.find(user => user.id === id);
 }
 
-//user leaves chat
+//user leaves chat 
 function userLeave(id){
     const index = users.findIndex(user => user.id === id);
 
